8a8326b9f20fc9501c3d3c1f5e6131d997cefbc2
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2021-04-07 15:42:13 +0100

    Added intro screen

--- src/Globals.gd
+++ src/Globals.gd
@@ -1,13 +1,13 @@
 extends Node
 
 const VERSION = "1"
-const RELEASE_MODE = false
+const RELEASE_MODE = true
 const DEBUG_START_POS = false and !RELEASE_MODE # If true, uses StartPos in main for player's start position
 const NO_ENEMIES = false and !RELEASE_MODE
 const SHOW_FPS = true and !RELEASE_MODE
 const PLAYER_INVINCIBLE = false and !RELEASE_MODE
 
-const START_TIME_SECONDS = 600
+const START_TIME_SECONDS = 250
 
 var rnd : RandomNumberGenerator
 
