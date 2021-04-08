81beb4872f241cb7f5f03f16d506e2fbea773db8
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2021-04-08 20:39:26 +0100

    Fixed tiny explosions

--- src/Explosion.gd
+++ src/Explosion.gd
@@ -4,15 +4,6 @@ extends Spatial
 func _ready():
 	if $AudioStreamPlayer:
 		$AudioStreamPlayer.play()
-
-	var tween : Tween = $Tween
-	if tween:
-		tween.interpolate_property($OmniLight, "light_energy",
-			1.0, 0.0, 
-			4,
-			Tween.TRANS_SINE, Tween.EASE_IN )
-		tween.repeat = false
-		tween.start()
 	pass
 	
 	
